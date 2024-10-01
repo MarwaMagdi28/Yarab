df67cd0b188a78c5c3e6b4a14839c44274c25af0
MarwaMagdi28 <moro.magdi@gmail.com>
2024-10-01 13:18:23 +0300

    zzzzzzzzzzz

--- main.tf
+++ main.tf
@@ -14,36 +14,8 @@ provider "aws" {
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-047d7c33f6e7b4bc4"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.prometheus_grafana_sg.id]
 
   tags = {
     Name = "MyEC2Instance"
-  }
-}
-
-resource "aws_security_group" "prometheus_grafana_sg" {
-  name = "prometheus-grafana-sg"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["13.52.6.112/29"]
-  }
-  
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
   }
 }

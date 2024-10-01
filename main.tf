10fc56c40d88bf0b37ff723b95f2035714ee8c0b
MarwaMagdi28 <moro.magdi@gmail.com>
2024-10-01 12:23:58 +0300

    1

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1" # Replace with your desired AWS region
+  # Add other AWS provider configuration as needed
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-047d7c33f6e7b4bc4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}

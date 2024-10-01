c868eac362f016d94fa057b89418bef63a7e7d86
MarwaMagdi28 <moro.magdi@gmail.com>
2024-10-01 15:47:36 +0300

    ssss

--- main.tf
+++ main.tf
@@ -20,11 +20,63 @@ resource "aws_instance" "ec2_instance" {
   }
 
   user_data = <<-EOF
-            #!/bin/bash
-            sudo yum update -y
-            sudo yum install -y wget curl git
-            curl -s https://prometheus.io/prometheus-2.43.0.linux-amd64.tar.gz -o prometheus.tar.gz
-            sudo tar -xzvf prometheus.tar.gz -C /usr/local/bin/
-            sudo chown -R ec2-user:ec2-user /usr/local/bin/prometheus
+          #!/bin/bash
+          # Update the system
+          sudo yum update -y
+
+          # Create a Prometheus user
+          sudo useradd --no-create-home --shell /bin/false prometheus
+
+          # Create necessary directories
+          sudo mkdir /etc/prometheus
+          sudo mkdir /var/lib/prometheus
+
+          # Download Prometheus
+          cd /tmp
+          curl -LO https://github.com/prometheus/prometheus/releases/download/v2.41.0/prometheus-2.41.0.linux-amd64.tar.gz
+
+          # Extract Prometheus
+          tar -xvf prometheus-2.41.0.linux-amd64.tar.gz
+          cd prometheus-2.41.0.linux-amd64
+
+          # Move binaries to /usr/local/bin
+          sudo mv prometheus /usr/local/bin/
+          sudo mv promtool /usr/local/bin/
+
+          # Move configuration files
+          sudo mv prometheus.yml /etc/prometheus/
+          sudo mv consoles /etc/prometheus/
+          sudo mv console_libraries /etc/prometheus/
+
+          # Set ownership
+          sudo chown -R prometheus:prometheus /etc/prometheus
+          sudo chown -R prometheus:prometheus /var/lib/prometheus
+
+          # Create a systemd service file for Prometheus
+          sudo tee /etc/systemd/system/prometheus.service <<EOF
+          [Unit]
+          Description=Prometheus
+          Wants=network-online.target
+          After=network-online.target
+
+          [Service]
+          User=prometheus
+          Group=prometheus
+          Type=simple
+          ExecStart=/usr/local/bin/prometheus \
+            --config.file /etc/prometheus/prometheus.yml \
+            --storage.tsdb.path /var/lib/prometheus/ \
+            --web.console.templates=/etc/prometheus/consoles \
+            --web.console.libraries=/etc/prometheus/console_libraries
+
+          [Install]
+          WantedBy=multi-user.target
+          EOF
+
+          # Reload systemd and start Prometheus
+          sudo systemctl daemon-reload
+          sudo systemctl start prometheus
+          sudo systemctl enable prometheus
+
           EOF
 }
